53d3c094b7fd2aa342f4d2346439bcca02bea23e
Roosevelt <58699961+RooseveltTech@users.noreply.github.com>
2023-11-25 11:52:09 +0100

    Update urls.py

--- crescoin/urls.py
+++ crescoin/urls.py
@@ -43,5 +43,6 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('auth/', include('core.urls')),
+    path('v1/api/', include('main.urls')), 
     path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
